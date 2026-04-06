287d5803ec515d972d0e27394b33e2e3cbceaf49
Het <hra.opash@gmail.com>
2026-04-06 15:49:34 +0530

    feat: implement pagination and metadata for fetching posts

--- Prisma-Express/controller/postController.js
+++ Prisma-Express/controller/postController.js
@@ -18,8 +18,21 @@ export const createPost = async (req, res) => {
 };
 
 export const getAllPosts = async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  if (page <= 0)
+    return res.status(400).json({ message: "Page  must be greater than 0" });
+
+  if (limit <= 0 || limit > 100) {
+    limit = 10;
+  }
+  const skip = (page - 1) * limit;
+
   try {
     const posts = await prisma.post.findMany({
+      skip: skip,
+      take: limit,
+
       include: {
         comments: {
           include: { user: true },
@@ -46,9 +59,18 @@ export const getAllPosts = async (req, res) => {
       //   ],
       // },
     });
-    return res
-      .status(200)
-      .json({ message: "Posts fetched successfully", posts });
+
+    const totalPosts = await prisma.post.count();
+    const totalPages = Math.ceil(totalPosts / limit);
+    return res.status(200).json({
+      message: "Posts fetched successfully",
+      posts,
+      meta: {
+        totalPages,
+        currentPage: page,
+        limit,
+      },
+    });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
