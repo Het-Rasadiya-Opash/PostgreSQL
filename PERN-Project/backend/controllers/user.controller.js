d8659c7d4e2cb223badd5dc8c398f6d912e53f6f
Het <hra.opash@gmail.com>
2026-04-08 11:24:55 +0530

    login api and routes

--- PERN-Project/backend/controllers/user.controller.js
+++ PERN-Project/backend/controllers/user.controller.js
@@ -52,11 +52,50 @@ export const registerUser = async (req, res) => {
 
     return res.status(201).json({
       message: "User created successfully",
-      data: newUser,
+      data: {
+        newUser,
+        token,
+      },
     });
   } catch (error) {
     return res.status(500).json({
       message: "Error creating user",
+      error: error.message,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+    res.cookie("token", token, { httpOnly: true });
+    return res.status(200).json({
+      message: "User logged in successfully",
+      data: { user, token },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error logging in user",
       error: error.message,
     });
   }
--- PERN-Project/backend/routes/user.route.js
+++ PERN-Project/backend/routes/user.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getAllUsers, registerUser } from "../controllers/user.controller.js";
+import {
+  getAllUsers,
+  loginUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
