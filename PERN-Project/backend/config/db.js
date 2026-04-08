14f409302c769684c870631bb3aaab6627ffd374
Het <hra.opash@gmail.com>
2026-04-08 10:20:00 +0530

    PERN Project - Porject Management Clone Frontend and Backend & PostgreSQL connection

---
+++ PERN-Project/backend/config/db.js
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
+
+export async function checkDatabaseConnection() {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    console.log(`Database connection Successfully...`);
+    return true;
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    return false;
+  }
+}
